765bdca9aeaf2745300c19f6ab8922645a57d1f1
Sergei Demianenko <dhusara@icloud.com>
2023-01-21 16:50:09 +0100

    Initial Commit

---
+++ LDITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LDITest
+//
+//  Created by Sergei Demianenko on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
